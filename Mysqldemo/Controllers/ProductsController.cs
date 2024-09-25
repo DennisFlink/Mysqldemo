95f0e4201601276dd4b80faff7eec04f6e86ac32
DennisFlink <dennis.sabriev@outlook.com>
2024-09-25 22:20:59 +0200

    Add project files.

---
+++ Mysqldemo/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mysqldemo.Data;
+using Mysqldemo.Models;
+
+namespace Mysqldemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+    }
+}
